881512755a61bca6c9e1fdd245f85277a80cc8b0
Stephen Brile <stephen.brile@gmail.com>
2015-03-29 20:19:47 -0400

    making compare better

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('nodeJS works again');
+  res.send('Briles Test');
 });
  
 app.listen(process.env.PORT || 5000);
